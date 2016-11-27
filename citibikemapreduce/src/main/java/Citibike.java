e843daec3c7b2a35755a80322831ec92c8e22a54
Jay Price <jared.x.price@gmail.com>
2016-11-27 01:47:56 -0500

    Changes to citibike jar.

--- citibikemapreduce/src/main/java/Citibike.java
+++ citibikemapreduce/src/main/java/Citibike.java
@@ -6,22 +6,26 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-public class Citibike {
- public static void main(String[] args) throws Exception {
- if (args.length != 2) {
- System.err.println("Usage: Citibike <input path> <output path>");
- System.exit(-1);
- }
- Job job = new Job();
- job.setJarByClass(Citibike.class);
- job.setJobName("Citibike");
- FileInputFormat.addInputPath(job, new Path(args[0]));
- FileOutputFormat.setOutputPath(job, new Path(args[1]));
- job.setMapperClass(CitibikeMapper.class);
- job.setReducerClass(CitibikeReducer.class);
- job.setOutputKeyClass(Text.class);
- job.setOutputValueClass(IntWritable.class);
- System.exit(job.waitForCompletion(true) ? 0 : 1);
- }
+public class Citibike 
+{
+    public static void main(String[] args) throws Exception 
+    {
+        if (args.length != 2) 
+        {
+            System.err.println("Usage: Citibike <input path> <output path>");
+            System.exit(-1);
+        }
+
+        Job job = new Job();
+        job.setJarByClass(Citibike.class);
+        job.setJobName("Citibike");
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        job.setMapperClass(CitibikeMapper.class);
+        job.setReducerClass(CitibikeReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
 }
 // ^^ Citibike
--- citibikemapreduce/src/main/java/CitibikeMapper.java
+++ citibikemapreduce/src/main/java/CitibikeMapper.java
@@ -9,27 +9,26 @@ public class CitibikeMapper
     extends Mapper<LongWritable, Text, Text, IntWritable> 
     {
     @Override
-        public void map(LongWritable key, Text value, Context context)
+        public void map(LongWritable key, IntWritable value, Context context)
         throws IOException, InterruptedException 
         {
             String line = value.toString();
-    
-            CSVReader R = new CSVReader(new StringReader(line));
+            comma = ",";
+            String[] ParsedLine = value.split(comma);
+            
+            if(ParsedLine.length == 15)
+            {
+                String date;
+                String origin;
+                String triplength;
 
-            String[] ParsedLine = R.readNext();
-            R.close();
-
-            String date;
-            String origin;
-            int triplength;
-
-
-            date = ParsedLine[1].substring(0, 10);
-            origin = ParsedLine[3]+","+ParsedLine[7]+","+date);
-            triplength = Integer.parseInt(ParsedLine[0]);
-
-            context.write(new Text(origin), new IntWritable(triplength));
+                date = ParsedLine[1].substring(0, 10);
+                origin = ParsedLine[3]+","+ParsedLine[7]+","+date);
+                triplength = Integer.parseInt(ParsedLine[0]);
+                context.write(new Text(origin), new IntWritable(triplength));
+            }
+                else
+                {}
         }
     }
-}
 
