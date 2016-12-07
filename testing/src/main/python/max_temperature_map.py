3ab99b18059a12d396fd9a7e4d9656a9ae97e37f
Jay Price <jared.x.price@gmail.com>
2016-12-07 12:00:21 -0500

    Final output for Citibike data.

--- testing/src/main/python/max_temperature_map.py
+++ testing/src/main/python/max_temperature_map.py
@@ -8,7 +8,7 @@ import sys
 
 for line in sys.stdin:
     val = line.strip()
-    (year, temp, q) = (val:15:19, val[87:92], val[92:93])
+    (year, temp, q) = (val[15:19], val[87:92], val[92:93])
     if(temp != "+9999" and re.match("[01459]", q)):
-        print "%s\t%s" % (year, temp)
+        print("%s\t%s" % (year, temp))
 
--- testing/src/main/python/max_temperature_reduce.py
+++ testing/src/main/python/max_temperature_reduce.py
@@ -1,27 +1,16 @@
-// cc MaxTemperatureMapper Mapper for maximum temperature example
-// vv MaxTemperatureMapper
-import java.io.IOException;
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.LongWritable;
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Mapper;
-public class MaxTemperatureMapper
- extends Mapper<LongWritable, Text, Text, IntWritable> {
- private static final int MISSING = 9999;
- @Override
- public void map(LongWritable key, Text value, Context context)
- throws IOException, InterruptedException {
- String line = value.toString();
- String year = line.substring(15, 19);
- int airTemperature;
- if (line.charAt(87) == '+') { // parseInt doesn't like leading plus signs
- airTemperature = Integer.parseInt(line.substring(88, 92));
- } else {
- airTemperature = Integer.parseInt(line.substring(87, 92));
- }
- String quality = line.substring(92, 93);
- if (airTemperature != MISSING && quality.matches("[01459]")) {
- context.write(new Text(year), new IntWritable(airTemperature));
- }
- }
-}
+#!/usr/bin/env python
+# cc MaxTemperatureMapper Mapper for maximum temperature example
+# vv MaxTemperatureMapper
+
+import sys
+(last_key, max_val) = (None, -sys.maxsize)
+for line in sys.stdin:
+    (key, val) = line.strip().split("\t")
+    if last_key and last_key != key:
+        print("%s\t%s" % (last_key, max_val))
+        (last_key, max_val) = (key, int(val))
+    else:
+        (last_key, max_val) = (key, max(max_val, int(val)))
+
+if last_key:
+    print("%s\t%s" % (last_key, max_val))
